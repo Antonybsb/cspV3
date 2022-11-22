99634706710d96917db2beb7d9670dc7bd823f48
Antonybsb <artbsb1@gmail.com>
2022-11-22 11:25:37 -0300

    Api para listar serviços

--- src/main/java/io/csp/rest/ServicoPrestadoController.java
+++ src/main/java/io/csp/rest/ServicoPrestadoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -48,5 +49,13 @@ public class ServicoPrestadoController {
         servicoPrestado.setValor( bigDeceimalConverter.converter(dto.getPreco()) );
 
         return repository.save(servicoPrestado);
+    }
+
+    @GetMapping
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam(value = "mes", required = false) Integer mes
+    ) {
+        return repository.findByNomeClienteAndMes("%" + nome + "%", mes);
     }
 }
