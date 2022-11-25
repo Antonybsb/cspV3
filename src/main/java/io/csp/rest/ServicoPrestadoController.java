cc45bd304c7f9bf1cf33dd3418c7ab224aea50a9
Antonybsb <artbsb1@gmail.com>
2022-11-25 11:19:03 -0300

    Api para listar serviços

--- src/main/java/io/csp/rest/ServicoPrestadoController.java
+++ src/main/java/io/csp/rest/ServicoPrestadoController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
--- src/main/java/io/csp/rest/dto/servicoPrestadoDTO.java
+++ src/main/java/io/csp/rest/dto/servicoPrestadoDTO.java
@@ -3,12 +3,21 @@ package io.csp.rest.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 public class servicoPrestadoDTO {
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotEmpty(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 
 }
